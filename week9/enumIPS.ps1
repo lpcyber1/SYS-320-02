7822fde397bb3736c956037630085e481fe8f7ea
Liam Paul <liam.paul@mymail.champlain.edu>
2023-11-01 21:21:33 -0400

    Week 9 Homework

--- week9/enumIPS.ps1
+++
@@ -1,4 +0,0 @@
-#Storyline: Enumerating DHCP and DNS server IPs
-
-#Getting DHCP, DNS, DHCP, IP Address
-Get-WmiObject Win32_NetworkAdapterConfiguration | Select DHCPServer, DNSServerSearchOrder, IPAddress, DefaultIPGateway |  Where { $_.DHCPServer, $_.DNSServerSearchOrder, $_.IPAddress, $_.DefaultIPGateway }
